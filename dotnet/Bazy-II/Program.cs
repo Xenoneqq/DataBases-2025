f5a5ef1e423d75e71aa15cafea1c5c2bb03f2705
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2025-05-14 13:25:13 +0200

    dotnet

---
+++ dotnet/Bazy-II/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+ProdContext prodContext
+= new ProdContext();
+Product product =
+new Product { ProductName
+'odContext . Products . Add ( product) ;
+prodContext . SaveChanqes( ) ,
+= ” Flamaster”
+
+public class Product {
+    public int ProductID {get; set;}
+    public String? ProductName {get; set;} 
+    public int UnitsInStock {get; set;}
+}
+
+
