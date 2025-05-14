43b4cdca02d8b8b094ba46db76ea3136a98b07a9
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2025-05-14 13:30:55 +0200

    yy

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,18 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-ProdContext prodContext
-= new ProdContext();
-Product product =
-new Product { ProductName
-'odContext . Products . Add ( product) ;
-prodContext . SaveChanqes( ) ,
-= ” Flamaster”
+ProdContext prodContext = new ProdContext();
+Product product = new Product { ProductName = "Flamaster"};
+prodContext.Products.Add(product);
+prodContext.SaveChanges();
 
 public class Product {
     public int ProductID {get; set;}
     public String? ProductName {get; set;} 
     public int UnitsInStock {get; set;}
 }
+
+
 
 
