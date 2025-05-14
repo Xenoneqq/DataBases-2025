e42095f3cf737884ced9b30b46594951aa4124b1
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2025-05-14 13:47:16 +0200

    first part done

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,10 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Linq;
+
 ProdContext prodContext = new ProdContext();
-Product product = new Product { ProductName = "Flamaster"};
+Console.WriteLine("Podaj nazwę produktu: ");
+String? prodName = Console.ReadLine();
+Product product = new Product { ProductName = prodName};
 prodContext.Products.Add(product);
 prodContext.SaveChanges();
+var query = from prod in prodContext.Products
+select prod.ProductName;
+
+foreach (var pName in query)
+{
+    Console.WriteLine(pName);
+}
+
+
 
 public class Product {
     public int ProductID {get; set;}
